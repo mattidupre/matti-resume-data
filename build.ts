f08d51a2c41c320c395bfcd3491db94a6550e0d6
Matti Dupre <matti.dupre@gmail.com>
2024-02-15 05:56:57 -0800

    Snapshot

--- build.ts
+++ build.ts
@@ -1,15 +1,28 @@
+import path from 'path';
 import fs from 'node:fs/promises';
 import { parse as parseYaml } from 'yaml';
 
-const resumeRaw = parseYaml(
-  await fs.readFile('./src/resume.yaml', {
-    encoding: 'utf8',
+const SRC_DIR = './src';
+const DIST_DIR = './dist-data';
+await fs.mkdir(DIST_DIR, { recursive: true });
+
+const files = await fs.readdir(SRC_DIR);
+
+await Promise.all(
+  files.map(async (file) => {
+    const base = path.parse(file).name;
+
+    const resumeRaw = parseYaml(
+      await fs.readFile(path.join(SRC_DIR, file), {
+        encoding: 'utf8',
+      })
+    );
+
+    await fs.writeFile(
+      path.join(DIST_DIR, `${base}.json`),
+      JSON.stringify(resumeRaw, undefined, 2)
+    );
+
+    console.log(`${base} built`);
   })
-);
-
-await fs.mkdir('./dist-data', { recursive: true });
-
-await fs.writeFile(
-  './dist-data/resume.json',
-  JSON.stringify(resumeRaw, undefined, 2)
 );
